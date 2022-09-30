4e5a797655fc435be75842ec12048c7894308cbd
ramnet04 <ram_net04@yahoo.co.in>
2022-10-01 00:46:23 +0530

    first commit for JenkinsDemo

---
+++ JenkinsDemo3.java
@@ -0,0 +1,10 @@
+
+public class JenkinsDemo3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Run when git commit is happening");
+	}
+
+}
+  
