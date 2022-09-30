884481dbc75b0a5fc75603b877485774c79b8569
ramnet04 <37320802+ramnet04@users.noreply.github.com>
2022-10-01 01:36:05 +0530

    Update JenkinsDemo3.java

--- JenkinsDemo3.java
+++ JenkinsDemo3.java
@@ -4,6 +4,7 @@ public class JenkinsDemo3 {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Run when git commit is happening");
+		System.out.println("1 Commit and push");
 	}
 
 }
