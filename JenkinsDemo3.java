c99a9bce4e55eb213c3523682dfe9c49efa61c5a
ramnet04 <37320802+ramnet04@users.noreply.github.com>
2022-10-01 01:38:35 +0530

    2 commit and push

--- JenkinsDemo3.java
+++ JenkinsDemo3.java
@@ -4,7 +4,7 @@ public class JenkinsDemo3 {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Run when git commit is happening");
-		System.out.println("1 Commit and push");
+		System.out.println("2 Commit and push");
 	}
 
 }
